7c92435152128e9bf642e103de707c0670612335
jianghongwei <jianghongwei@58ganji.com>
2017-03-20 14:18:23 +0800

    init

--- index.ios.js
+++ index.ios.js
@@ -12,6 +12,42 @@ import {
   View
 } from 'react-native';
 
-import App from './app/index';
+export default class swiper extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
 
-AppRegistry.registerComponent('Swiper', () => App);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('swiper', () => swiper);
